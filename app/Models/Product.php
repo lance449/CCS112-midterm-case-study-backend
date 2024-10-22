2b56933cffcd9ef4de6f59f8bec736eb502e1e31
NavarrozaYvan <navarrozayvanjade52@gmail.com>
2024-10-22 17:26:56 +0800

    Update Product.php
    enhanced the product.php by allowing mass assignment for specified fields, making it more functional for handling data input.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,16 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Specify the fields that are mass assignable
+    protected $fillable = [
+        'barcode',
+        'description',
+        'price',
+        'quantity',
+        'category',
+    ];
 }
