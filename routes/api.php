ac2643ce5526d3532a13c3d748d3a292b1dda577
Mark Joseph Malvar <149479021+itsmemarkmalvar@users.noreply.github.com>
2024-10-23 10:10:34 +0800

    Logout function Done

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::apiResource('products', ProductController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
